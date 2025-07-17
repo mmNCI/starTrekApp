9a35964eda0ee652d530804221a52bd1c31a9aa1
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-07-17 19:15:00 +0100

    fix: removed version check of ruby and bundle in sh

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ docker compose down
 docker compose build
 docker compose up -d
 
-which ruby
-which bundle
-ruby -v
-bundle -v
+# which ruby
+# which bundle
+# ruby -v
+# bundle -v
 
 docker exec rails db:migrate RAILS_ENV=production
 docker exec rails assets:precompile RAILS_ENV=production
